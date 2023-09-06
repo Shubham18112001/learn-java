aebe84dbad3030eb8debd286aa5b0577136c1123
Shubham <shubham61690@gmail.com>
2023-09-06 11:30:32 +0530

    adding p1 java file

---
+++ p1.java
@@ -0,0 +1,10 @@
+public class p1 {
+    public static void main(String[] args){
+        String name = "Aman";
+        int age = 30;
+        String neighbour = "Shubham";
+        String friend = neighbour;
+        System.out.println(name + " " + age + " " + neighbour + " " + friend);
+        System.out.println(name.length());
+    }
+}
