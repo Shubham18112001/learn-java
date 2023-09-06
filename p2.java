050518c053004493ab0796391f72054b4184fc51
Shubham <shubham61690@gmail.com>
2023-09-06 11:31:04 +0530

    adding p2.java file

---
+++ p2.java
@@ -0,0 +1,13 @@
+
+public class p2 {
+      public static void main(String[] args){
+        //String name = "Aman";
+        String names = new String( "Shubham");
+        //String friend = new String( "Aman");
+        System.out.println(names.length());
+        String name1 = "abcde";
+        String name2 = "fghijkl";
+        String name3 = name1 + " and " + name2;
+        System.out.println(name3);
+      }
+}
