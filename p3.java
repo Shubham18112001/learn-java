ef7b8d427afe63bf8a811edf5f60f7c61d5ed4ba
Shubham <shubham61690@gmail.com>
2023-09-06 11:32:03 +0530

    adding p3 and p4 java file

---
+++ p3.java
@@ -0,0 +1,11 @@
+public class p3 {
+      public static void main(String[] args){
+        String name = "Aman";
+        //int age = 30;
+        System.out.println(name.charAt(0));
+        String name1 = "Shubham";
+        String name2 = name1.replace('a','b');
+        System.out.println(name2);
+       
+      }
+}
---
+++ p4.java
@@ -0,0 +1,16 @@
+public class p4 {
+     public static void main(String[] args){
+       
+        //int age = 30;
+        //int physics = 45;
+        //int chemistry = 40;
+        //int mathematics = 90;
+
+        boolean[] marks = new boolean[3];
+        //marks[0] = 45;
+        //marks[1] = 40;
+        //marks[2] = 90;
+        System.out.println(marks[2]);
+
+    }
+}
